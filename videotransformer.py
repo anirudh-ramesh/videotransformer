9eb93661dc2148db45d10abc95b07f0cfa9694a9
Alexandre J. Raymond <alexandre.raymond@lacime.etsmtl.ca>
2014-12-09 13:37:10 -0500

    videotransformer: implement salt and pepper noise

--- videotransformer.py
+++ videotransformer.py
@@ -3,6 +3,7 @@
 import cv2
 import sys
 import argparse
+import numpy as np
 
 
 def nop(*args):
@@ -17,6 +18,7 @@ def main():
     parser.add_argument('--mirrorh', action='store_true', help='Mirror image horizontally')
     parser.add_argument('--mirrorv', action='store_true', help='Mirror image vertically')
     parser.add_argument('--noise-gaussian', default='0,0', metavar='M,D', help='Add gaussian noise')
+    parser.add_argument('--noise-saltpepper', default='0,0', metavar='M,D', help='Add gaussian noise')
     parser.add_argument('--speedup', type=int, default=1, choices=xrange(1,5), help='Speed-up playback (integer factor)')
     args = parser.parse_args()
     print repr(args)
@@ -35,6 +37,12 @@ def main():
         noise_gaussian = [int(i) for i in noise_gaussian]
     except ValueError:
         noise_gaussian = (0.,0.)
+        
+    try:
+        noise_saltpepper = args.noise_saltpepper.split(',')
+        noise_saltpepper = [int(i) for i in noise_saltpepper]
+    except ValueError:
+        noise_saltpepper = (0.,0.)
     ###########################
 
     cap = cv2.VideoCapture(args.vid_in)
@@ -102,14 +110,33 @@ def main():
 
         #
         if noise_gaussian[0] and noise_gaussian[1]:
-            
             chans = cv2.split(outputImage)
             noiseImage = chans[0].copy()
-            cv2.randn(noiseImage,noise_gaussian[0],noise_gaussian[1])
+            cv2.randn(noiseImage, noise_gaussian[0], noise_gaussian[1])
             chans[0] = cv2.add(chans[0], noiseImage)
             chans[1] = cv2.add(chans[1], noiseImage)
             chans[2] = cv2.add(chans[2], noiseImage)
             outputImage = cv2.merge(chans)
+        
+        #
+        if noise_saltpepper[0] and noise_saltpepper[1]:
+            chans = cv2.split(outputImage)
+            noiseImage = chans[0].copy()
+            cv2.randu(noiseImage, 0, 255)
+            black = noiseImage < noise_saltpepper[0]
+            black = black * 255
+            black = black.astype(np.uint8)
+            white = noiseImage > noise_saltpepper[1]
+            white = white * 255
+            white = black.astype(np.uint8)
+            chans[0] = cv2.subtract(chans[0], black)
+            chans[0] = cv2.add(chans[0], white)
+            chans[1] = cv2.subtract(chans[1], black)
+            chans[1] = cv2.add(chans[1], white)
+            chans[2] = cv2.subtract(chans[2], black)
+            chans[2] = cv2.add(chans[2], white)
+            outputImage = cv2.merge(chans)
+            
         #
         if args.speedup > 1 and i % args.speedup > 0:
             keptFrame = False
